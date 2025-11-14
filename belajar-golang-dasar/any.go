5dd2228be7b49e95f83615d18eea65da0a0dd8f2
muharromi <muharromi42@gmail.com>
2025-11-14 09:04:32 +0700

    any, nil

---
+++ belajar-golang-dasar/any.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+// interface kosong
+func Upss() any {
+	// return 1
+	// return true
+	return "upss"
+}
+
+func main() {
+	var kosong any = Upss()
+	fmt.Println(kosong)
+
+}
